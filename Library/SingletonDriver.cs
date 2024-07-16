b3d5ba03805a3792fb838a2a73ea1b56ff715884
yusuftriwibowo000 <Yusuf.Tri.Wibowo@bni.co.id>
2024-07-16 11:31:09 +0700

    First push add project selenium c# (Android Base)

---
+++ Library/SingletonDriver.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium;
+using LibraryPDF;
+
+namespace SeleniumNew
+{
+    public sealed class SingletonDriver
+    {
+        private static AndroidDriver driver;
+
+        private SingletonDriver() { }
+
+        public static AndroidDriver GetDriver()
+        {
+            if (driver == null)
+            {
+                //String pathApp = "C:\\Users\\HP\\Documents\\MobileTesting\\flutter-app (1).apk";
+                String pathApp = LibPDF.projectDir + "/flutter-app (1).apk";
+
+                // Desired Capabilities
+                AppiumOptions options = new AppiumOptions();
+                options.AddAdditionalAppiumOption(MobileCapabilityType.PlatformName, "Android");
+                options.DeviceName = "1319da2d";
+                options.App = pathApp;
+                options.AddAdditionalAppiumOption("appPackage", "io.flutter.demo.gallery");
+                options.AddAdditionalAppiumOption("appActivity", "io.flutter.demo.gallery.MainActivity");
+
+                // Initialize the driver
+                driver = new AndroidDriver(new Uri("http://localhost:4723/wd/hub"), options);
+            }
+            return driver;
+        }
+    }
+}
---
+++ TC0001_AksesMenuStudies.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Appium.Android;
+using SeleniumNew;
+using LibraryPDF;
+using GlobalLibrary;
+
+namespace MobileTesting
+{
+    class Testing
+    {
+        public static AndroidDriver driver = SingletonDriver.GetDriver();
+        public static string excelFilePath = LibPDF.projectDir + "/Excel/TC0001_AksesMenuStudies.xlsx";
+        public static string excelSheetName = "TC0001";
+        static void Main(string[] args)
+        {
+            LibPDF.InitializeDocument(excelFilePath, excelSheetName); // Initialize document before tests
+            LibPDF.CreateCover();
+
+            LibGlobal.AksesMenuStudies();
+
+            LibPDF.GeneratePDF(excelFilePath, excelSheetName);
+            driver.Quit();
+        }
+    }
+}
